b266e6a48588de30d1f52914ad7ffcc3c33c4744
sarah <sarah@thegivingchain.com>
2022-06-20 09:00:00 -0500

    Add cohort analysis endpoint

--- src/routes/analytics.ts
+++ src/routes/analytics.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { analyzeFunnel, FunnelQuery } from '../services/funnel';
+import { analyzeCohort, CohortQuery } from '../services/cohort';
 
 const router = Router();
 
@@ -51,6 +52,72 @@ router.post('/funnel', async (req: Request, res: Response, next: NextFunction) =
     };
     
     const result = await analyzeFunnel(query);
+    
+    res.json({
+      success: true,
+      data: result
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * POST /api/analytics/cohort
+ * Analyze user retention by cohort
+ */
+router.post('/cohort', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { cohortEvent, returnEvent, startDate, endDate, granularity, periods } = req.body;
+    
+    // Validate required fields
+    if (!cohortEvent || typeof cohortEvent !== 'string') {
+      return res.status(400).json({
+        error: 'Invalid request',
+        message: 'cohortEvent is required and must be a string'
+      });
+    }
+    
+    if (!returnEvent || typeof returnEvent !== 'string') {
+      return res.status(400).json({
+        error: 'Invalid request',
+        message: 'returnEvent is required and must be a string'
+      });
+    }
+    
+    if (!startDate || !endDate) {
+      return res.status(400).json({
+        error: 'Invalid request',
+        message: 'startDate and endDate are required'
+      });
+    }
+    
+    const validGranularities = ['day', 'week', 'month'];
+    if (granularity && !validGranularities.includes(granularity)) {
+      return res.status(400).json({
+        error: 'Invalid request',
+        message: 'granularity must be one of: day, week, month'
+      });
+    }
+    
+    const periodsValue = periods ? parseInt(periods, 10) : 12;
+    if (isNaN(periodsValue) || periodsValue < 1 || periodsValue > 52) {
+      return res.status(400).json({
+        error: 'Invalid request',
+        message: 'periods must be a number between 1 and 52'
+      });
+    }
+    
+    const query: CohortQuery = {
+      cohortEvent,
+      returnEvent,
+      startDate,
+      endDate,
+      granularity: granularity || 'week',
+      periods: periodsValue
+    };
+    
+    const result = await analyzeCohort(query);
     
     res.json({
       success: true,
